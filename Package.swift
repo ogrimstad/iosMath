3ccd6bcd040593b0d7cb9bb0e13bcb019c8602cc
Oystein Grimstad <oystein@getkahoot.com>
2020-09-15 15:15:38 +0200

    Initial SPM package file

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "IosMath",
+    products: [
+        .library(
+            name: "IosMath",
+            targets: ["IosMath"]),
+    ],
+    targets: [
+        .target(
+            name: "IosMath",
+            path: "IosMath",
+            dependencies: []),
+    ]
+)
